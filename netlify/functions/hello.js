ff27e86ce6002abbd137ef950132aceef38fd2bd
Stacey <dropdeadthread@gmail.com>
2025-10-21 18:49:05 -0400

    Add simple hello function to test deployment

---
+++ netlify/functions/hello.js
@@ -0,0 +1,14 @@
+exports.handler = async (event) => {
+  return {
+    statusCode: 200,
+    headers: {
+      'Content-Type': 'application/json',
+      'Access-Control-Allow-Origin': '*',
+    },
+    body: JSON.stringify({
+      message: 'Hello from Netlify Functions!',
+      timestamp: new Date().toISOString(),
+      event: event.queryStringParameters,
+    }),
+  };
+};
