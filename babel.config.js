14a93a3f56c10b23bc87212d9f5a29446c07b283
Stacey <dropdeadthread@gmail.com>
2025-10-26 20:30:42 -0400

    Fix Babel/ESLint config: exclude Netlify functions from Babel parsing

--- babel.config.js
+++ babel.config.js
@@ -1,3 +1,8 @@
 module.exports = {
   presets: ['babel-preset-gatsby'],
+  ignore: [
+    // Exclude Netlify functions from Babel processing
+    // They use CommonJS and don't need transpilation
+    'netlify/functions/**/*',
+  ],
 };
